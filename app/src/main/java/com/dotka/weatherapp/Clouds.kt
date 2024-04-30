f5cd21820bb711acbb494791c94b4133544fb774
kangkan.jyoti.ray <jonnyliver47@gmail.com>
2024-04-30 18:41:53 +0530

    Adding project from the Alienware

---
+++ app/src/main/java/com/dotka/weatherapp/Clouds.kt
@@ -0,0 +1,5 @@
+package com.dotka.weatherapp
+
+data class Clouds(
+    val all: Int
+)
---
+++ app/src/main/java/com/dotka/weatherapp/Coord.kt
@@ -0,0 +1,6 @@
+package com.dotka.weatherapp
+
+data class Coord(
+    val lat: Double,
+    val lon: Double
+)
